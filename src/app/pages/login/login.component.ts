8f56a386b62644c54218580d7b0558f837f7c059
josemoreno <josel.moreno87@gmail.com>
2020-09-11 15:54:43 -0500

    Se agregan funcionalidades al login y logOut

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+// Sweet Alert
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioModel = new UsuarioModel();
+  recordarUsuario = false;
+  constructor( private auth: AuthService,
+               private router: Router) { }
 
   ngOnInit() {
+    if (localStorage.getItem('email') ) {
+      this.usuario.email = localStorage.getItem('email');
+      this.recordarUsuario = true;
+    }
+  }
+
+  onSubmit( form: NgForm ) {
+    if ( form.invalid ) {
+      return;
+    }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+
+    Swal.showLoading();
+
+    this.auth.logIn( this.usuario ).subscribe( resp => {
+      console.log(resp);
+      Swal.close();
+      if (this.recordarUsuario) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+      this.router.navigateByUrl('/home');
+      }, (err) => {
+        console.log(err.error.error.message);
+        Swal.fire({
+          icon: 'error',
+          text: err.error.error.message,
+          title: 'Error al autenticar'
+        });
+      });
   }
 
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+// Sweet Alert
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
+  recordarUsuario = false;
+  constructor( private auth: AuthService,
+               private router: Router ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
 
-  ngOnInit() { }
+  onSubmit( form: NgForm ) {
+    if ( form.invalid ) {
+      return;
+    }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+
+    this.auth.registrarNuevoUsario( this.usuario ).subscribe( resp => {
+        console.log(resp);
+        Swal.close();
+        if (this.recordarUsuario) {
+          localStorage.setItem('email', this.usuario.email);
+        }
+        this.router.navigateByUrl('/home');
+      }, (err) => {
+        console.log(err.error.error.message);
+        Swal.fire({
+          icon: 'error',
+          text: err.error.error.message,
+          title: 'Error al registrar'
+        });
+      });
+  }
 
 }
